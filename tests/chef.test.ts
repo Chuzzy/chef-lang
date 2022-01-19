41a05c94585a1fcf0366366732eb50c6422814a7
Chuzzy <thechuzzyguy@gmail.com>
2022-01-19 00:04:27 +0000

    Run Prettier on chef tests

--- tests/chef.test.ts
+++ tests/chef.test.ts
@@ -1,8 +1,8 @@
 import chef from "../src/chef";
 
 describe("The Chef Interpreter", () => {
-    it("bakes the Hello World Souffle", () => {
-        const recipe = `Hello World Souffle.
+  it("bakes the Hello World Souffle", () => {
+    const recipe = `Hello World Souffle.
 
 This recipe prints the immortal words "Hello world!", in a basically brute force way. It also makes a lot of food for one person.
 
@@ -20,8 +20,8 @@ Ingredients.
 Method.
 Put potatoes into the mixing bowl. Put dijon mustard into the mixing bowl. Put lard into the mixing bowl. Put red salmon into the mixing bowl. Put oil into the mixing bowl. Put water into the mixing bowl. Put zucchinis into the mixing bowl. Put oil into the mixing bowl. Put lard into the mixing bowl. Put lard into the mixing bowl. Put eggs into the mixing bowl. Put haricot beans into the mixing bowl. Liquefy contents of the mixing bowl. Pour contents of the mixing bowl into the baking dish.
 
-Serves 1.`
+Serves 1.`;
 
-        expect(chef(recipe)).toEqual("Hello world!")
-    })
+    expect(chef(recipe)).toEqual("Hello world!");
+  });
 });
